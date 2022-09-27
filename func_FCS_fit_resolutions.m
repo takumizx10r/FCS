51f581114ad9b1f366f6054399c4f38a8f0bd7db
takumizx10r <takumi.saitou.b2@tohoku.ac.jp>
2022-09-28 00:22:52 +0900

    new

---
+++ func_FCS_fit_resolutions.m
@@ -0,0 +1,83 @@
+function [x] = func_FCS_fit_resolutions(interval,photon,R)
+N_max=size(photon,2);
+w=0.5*R;
+prompt = {'Initial N:','Known D:', 'FitRange(frame):', 'Dimension:','Move sum:', 'Initial dz (um)'};
+dlgtitle = 'Initial values';
+dims = [1 35];
+definput = {'10','420', num2str(N_max), '3','1','10'};
+answer = inputdlg(prompt,dlgtitle,dims,definput);
+Ni=str2num(answer{1});
+Di=str2double(answer{2});
+FitRange=str2num(answer{3});
+dimension=str2num(answer{4});
+movesum=str2double(answer{5});
+Dz=str2double(answer{6});
+
+if movesum~=0
+    photon=movsum(photon,movesum);
+end
+
+ave_photon=mean(photon);
+photon=photon-ave_photon;
+tau=zeros(1,N_max);
+g=zeros(1,N_max);
+tic
+for j=0:N_max-1
+    keep=zeros(1,N_max);
+    keep(j+1:N_max)=photon(1:N_max-j).*photon(j+1:N_max);
+    tau(j+1)=interval*j;
+    %     g(j+1)=mean(keep);
+    g(j+1)=mean(keep(j+1:N_max));
+    %     clear keep
+end
+toc
+g = 1.0 + g./( ave_photon*ave_photon );
+
+% g=g./( mean(photon.*photon) );
+
+
+
+% % % Fitting
+disp('Fit Autocorrelation Function...');
+n=1;
+while n>0
+    if dimension==3
+        G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*Di.*time./w^2)./sqrt((1.0+4.0*Di.*time/x(2)/x(2)));
+    elseif dimension==2
+        G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*Di.*time./w^2);
+    end
+
+    x0=[Ni Dz];
+    [x,resnorm,~,exitflag,output] = lsqcurvefit(G,x0,tau(1:FitRange),g(1:FitRange));
+    disp(x);
+    semilogx(tau(1:FitRange),g(1:FitRange),'k*');
+    hold on
+    semilogx(tau(1:FitRange),G(x,tau(1:FitRange)),'b-');
+    hold off
+    %     ylim([-0.1 1.1]);
+
+    answer = questdlg('Do you like to fit again with different I.V.?', ...
+        'Yes or No', ...
+        'Yes','No','Cancel');
+    % Handle response
+    switch answer
+        case 'Yes'
+            prompt = {'Initial N:','Known D:', 'FitRange(frame):', 'Dimension:','Move sum:', 'Initial dz (um)'};
+            dlgtitle = 'Initial values';
+            dims = [1 35];
+            definput = {num2str(Ni),num2str(Di),num2str(FitRange),num2str(dimension),num2str(movesum),num2str(Dz)};
+            answer = inputdlg(prompt,dlgtitle,dims,definput);
+            Ni=str2num(answer{1});
+            Di=str2double(answer{2});
+            FitRange=str2num(answer{3});
+            dimension=str2num(answer{4});
+            movesum=str2double(answer{5});
+            Dz=str2double(answer{6});
+
+        case 'No'
+            n=0;
+    end
+
+end
+
+end
