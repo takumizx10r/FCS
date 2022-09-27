c9fbc9a3848cb7eb09df25685337ac8941ea1a78
takumizx10r <takumi.saitou.b2@tohoku.ac.jp>
2022-09-28 00:22:32 +0900

    update

--- FCS_experiment.m
+++ FCS_experiment.m
@@ -1,15 +1,15 @@
 clear
 close all
-dt=4.0*10^(-6);
+dt=2.0*10^(-6); %%sec
 MaxIter=1*10^5;
-Resolution=0.170;
+Resolution=0.170; %%um
 [imfile, path, indx]=uigetfile(strcat(pwd,'\*.tif'));
 imdata=imread(strcat(path,'/',imfile));
 
 if MaxIter==inf
     TotalIteration=size(imdata,1)*size(imdata,2)-1;
 elseif MaxIter>size(imdata,1)*size(imdata,2)
-    display('MaxIter exceeds image size.');
+    disp('MaxIter exceeds image size.');
     return;
 else
     TotalIteration=MaxIter;
@@ -22,10 +22,14 @@ end
 figure
 plot(tdata,N_photon)
 
-% % % Fitting Autocorrelation function
-display('Fitting Autocorrelation function')
-[FitPara]=func_FCS(dt,N_photon,Resolution,0.0);
+% % % Fitting Autocorrelation function to determine DZ
+[FitPara]=func_FCS_fit_resolutions(dt,N_photon,Resolution);
 N_detect=FitPara(1);
-D_eff=(Resolution/2.0)*(Resolution/2.0) / (4.0*FitPara(2));
+Dz=FitPara(2);
+
+% % % Fitting Autocorrelation function
+% [FitPara]=func_FCS(dt,N_photon,Resolution,0.60);
+% N_detect=FitPara(1);
+% D_eff=FitPara(2);
 % % % 
 cd (path);
--- func_FCS.m
+++ func_FCS.m
@@ -1,15 +1,20 @@
 function [x] = func_FCS(interval,photon,R,Dz)
 N_max=size(photon,2);
 w=0.5*R;
-
-prompt = {'Initial N:','Initial D:', 'FitRange(frame):'};
+prompt = {'Initial N:','Initial D:', 'FitRange(frame):', 'Dimension:','Move sum:'};
 dlgtitle = 'Initial values';
 dims = [1 35];
-definput = {'10','0.00001',num2str(N_max)};
+definput = {'10','100',num2str(N_max), '3','1'};
 answer = inputdlg(prompt,dlgtitle,dims,definput);
 Ni=str2num(answer{1});
-Di=str2num(answer{2});
+Di=str2double(answer{2});
 FitRange=str2num(answer{3});
+dimension=str2num(answer{4});
+movesum=str2double(answer{5});
+
+if movesum~=0
+    photon=movsum(photon,movesum);
+end
 
 ave_photon=mean(photon);
 photon=photon-ave_photon;
@@ -17,44 +22,52 @@ tau=zeros(1,N_max);
 g=zeros(1,N_max);
 tic
 for j=0:N_max-1
+    keep=zeros(1,N_max);
     keep(j+1:N_max)=photon(1:N_max-j).*photon(j+1:N_max);
     tau(j+1)=interval*j;
+    %     g(j+1)=mean(keep);
     g(j+1)=mean(keep(j+1:N_max));
-    clear keep
+    %     clear keep
 end
 toc
-g=1.0 + g./( ave_photon*ave_photon );
+g = 1.0 + g./( ave_photon*ave_photon );
 
-% AutoCor='1.0 + 1.0/a ./ (1+x/b)'
 % g=g./( mean(photon.*photon) );
-% % % Fitting 
-display('Fit Autocorrelation Function');
+% % % Fitting
+display('Fit Autocorrelation Function...');
 n=1;
 while n>0
-%     G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*x(2).*time./w^2).*sqrt(1.0./(1.0+4.0*x(2).*time./Dz^2));
-    G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*x(2).*time./w^2);
+    if dimension==3
+        G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*x(2).*time./w^2).*sqrt(1.0./(1.0+4.0*x(2).*time./Dz^2));
+    elseif dimension==2
+        G = @(x,time)  1.0 + 1.0/x(1) ./ (1+4.0*x(2).*time./w^2);
+    end
+
     x0=[Ni Di];
     [x,resnorm,~,exitflag,output] = lsqcurvefit(G,x0,tau(1:FitRange),g(1:FitRange));
-    semilogx(tau(1:FitRange),g(1:FitRange));
+    disp(x);
+    semilogx(tau(1:FitRange),g(1:FitRange),'k*');
     hold on
-    semilogx(tau(1:FitRange),G(x,tau(1:FitRange)));
+    semilogx(tau(1:FitRange),G(x,tau(1:FitRange)),'b-');
     hold off
-%     ylim([-0.1 1.1]);
+    %     ylim([-0.1 1.1]);
 
     answer = questdlg('Do you like to fit again with different I.V.?', ...
-    	'Yes or No', ...
-    	'Yes','No','Cancel');
+        'Yes or No', ...
+        'Yes','No','Cancel');
     % Handle response
     switch answer
         case 'Yes'
-            prompt = {'Initial N:','Initial tau_D:', 'FitRange(frame):'};
+            prompt = {'Initial N:','Initial D:', 'FitRange(frame):', 'Dimension:','Move sum:'};
             dlgtitle = 'Initial values';
             dims = [1 35];
-            definput = {num2str(Ni),num2str(Di),num2str(FitRange)};
+            definput = {num2str(Ni),num2str(Di),num2str(FitRange),num2str(dimension),num2str(movesum)};
             answer = inputdlg(prompt,dlgtitle,dims,definput);
             Ni=str2num(answer{1});
-            Di=str2num(answer{2});
+            Di=str2double(answer{2});
             FitRange=str2num(answer{3});
+            dimension=str2num(answer{4});
+            movesum=str2double(answer{5});
         case 'No'
             n=0;
     end
